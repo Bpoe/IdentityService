4fc3d105f0e750a41d52675c8b3a1541d941c8b2
Brandon Poe <bpoe@microsoft.com>
2019-12-13 11:38:12 -0800

    Adding readonly

--- src/Identity/Controllers/IdentityController.cs
+++ src/Identity/Controllers/IdentityController.cs
@@ -12,9 +12,9 @@
     [ApiController]
     public class TokenController : ControllerBase
     {
-        private static DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        private IOptions<MsiOptions> options;
+        private readonly IOptions<MsiOptions> options;
 
         public TokenController(IOptions<MsiOptions> options)
         {
