290481fffb6fd491f4a87500eb3cc86b2e6d5283
Brandon Poe <bpoe@microsoft.com>
2019-12-20 12:24:45 -0800

    Cleanup unused usings

--- src/Identity/Startup.cs
+++ src/Identity/Startup.cs
@@ -1,20 +1,12 @@
 ﻿namespace Identity
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Security.Cryptography.X509Certificates;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.HttpsPolicy;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
     using Microsoft.Extensions.Options;
     using Infrastructure;
-    using Interfaces;
     using Microsoft.Identity.Client;
 
     public class Startup
