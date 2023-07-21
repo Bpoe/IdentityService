658bffaf01e891c23518c8e4bbbec4498c9487c0
Brandon Poe <bpoe@microsoft.com>
2023-07-21 11:11:47 -0700

    Adding error handling for cache operations so that a problem with the cache doesn't cause the request to fail

--- src/Identity/TokenService.cs
+++ src/Identity/TokenService.cs
@@ -41,8 +41,8 @@ public class TokenService
 
         // Check for cached access token
         var key = $"{requestOptions.TenantId.ToLowerInvariant()}_{resource.ToLowerInvariant()}";
-        var cached = await this.cache.GetStringAsync(key);
-        if (cached != null)
+        var cached = await this.GetCachedStringAsync(key);
+        if (cached is not null)
         {
             this.logger?.LogDebug("Found cached access token.");
             return TokenResponse.FromString(cached);
@@ -55,15 +55,43 @@ public class TokenService
         var accessToken = await credential.GetTokenAsync(context, CancellationToken.None);
 
         // Add token to cache
-        this.logger?.LogDebug("Adding access token to cache.");
-        await this.cache.SetStringAsync(
-           key,
-           accessToken.Token,
-           new DistributedCacheEntryOptions
-           {
-               AbsoluteExpiration = accessToken.ExpiresOn - CacheBuffer,
-           });
+        await this.SetCachedAccessTokenAsync(key, accessToken);
 
         return TokenResponse.FromString(accessToken.Token);
+    }
+
+    public async Task<string?> GetCachedStringAsync(string key)
+    {
+        try
+        {
+            return await this.cache.GetStringAsync(key);
+        }
+        catch (Exception ex)
+        {
+            // Redis will throw if the connection fails. If this happens,
+            // we shouldn't crash, we should just get a new token.
+            this.logger?.LogError(ex, "An exception occurred while reading the cache.");
+            return null;
+        }
+    }
+
+    public async Task SetCachedAccessTokenAsync(string key, AccessToken accessToken)
+    {
+        this.logger?.LogDebug("Adding access token to cache.");
+
+        try
+        {
+            await this.cache.SetStringAsync(
+               key,
+               accessToken.Token,
+               new DistributedCacheEntryOptions
+               {
+                   AbsoluteExpiration = accessToken.ExpiresOn - CacheBuffer,
+               });
+        }
+        catch (Exception ex)
+        {
+            this.logger?.LogError(ex, "An exception occurred while setting the cache.");
+        }
     }
 }
